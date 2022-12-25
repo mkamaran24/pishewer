77b9c29f039fdce355077500c9c85ac7792f5a9a
SoftwareGroup2019 <mohamadit69@gmail.com>
2022-12-24 21:34:45 -0800

    ffff

---
+++ database/migrations/2022_12_24_021325_create_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("title");
+            $table->unsignedBigInteger('categ_id');
+            $table->unsignedBigInteger('subcateg_id');
+            $table->text("image");
+            $table->text('description');
+            $table->text('keyword');
+            $table->string("price",50);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
