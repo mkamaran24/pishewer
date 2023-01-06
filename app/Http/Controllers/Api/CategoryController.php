ef017b34ceec3e280129b7c3056c8685e9222667
SoftwareGroup2019 <mohamadit69@gmail.com>
2023-01-05 22:14:09 -0800

    CategoryDone

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,78 +4,179 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Category as CategoryResource;
-use App\Models\Category;
+use App\Models\Category as CategoryModel;
 use App\Models\Subcategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         //
-        try 
-        {
+        try {
 
-            return CategoryResource::collection(Category::all());
-
+            return CategoryResource::collection(CategoryModel::all());
         } catch (\Throwable $th) {
+            abort(code: 500, message: 'fail to fetch');
             //throw $th;
-            return response()->json([
-                'status'=>false,
-                'message'=>$th->getMessage(),
-            ],500);
+            // return response()->json([
+            //     'status'=>false,
+            //     'message'=>$th->getMessage(),
+            // ],500);
         }
     }
 
-
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
+        //Validations Rules //////////////////////////
+        $rules = array(
+            'name' => 'required',
+        );
+        /// end of Validation Rules ////////////////////
+
+        // Validator Check /////////////////////////////
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $messages = $validator->messages();
+            $errors = $messages->all(); //convert them into one array
+            return response()->json([
+                'status' => false,
+                'reason' => 'Validation Fails',
+                'messages' => $errors,
+            ], 422);
+        } else {
+            # put data to DB after Succes Validation
+            try {
+
+                // Save to DB ///////////////////////////////////////////
+                $sub_categ = CategoryModel::create([
+                    'name' => $request->name,
+                ]);
+                /////////////////////////////////////////////////////////
+
+                // return Job API Resource JSON Response //////////////
+                return new CategoryResource($sub_categ);
+                ///////////////////////////////////////////////////////
+
+            } catch (\Throwable $th) {
+                abort(code: 500, message: 'fail to create');
+                // //throw $th;
+                // return response()->json([
+                //     'status' => false,
+                //     'message' => $th->getMessage(),
+                // ], 500);
+            }
+        }
+        //// end of Validator Check ///////////////////////
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        try {
+            // Validation of $id should goes here
+
+
+            /////////////////////////////////////
+            $categ = CategoryModel::find($id);
+            if ($categ) {
+                return new CategoryResource($categ);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'messages' => "Object Not Found"
+                ], 404);
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            abort(code: 500, message: 'fail to find object');
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            // Validation of $id should goes here
+
+
+            /////////////////////////////////////
+
+            // Validation of $request should goes here
+
+            //Validations Rules //////////////////////////
+            $rules = array(
+                'name' => 'required',
+            );
+            /// end of Validation Rules ////////////////////
+
+            //Validation Custom Messages
+            // $messages = array('title'=>'All data required');
+
+
+            // Validator Check //////////////////////////////
+            $validator = Validator::make($request->all(), $rules);
+            if ($validator->fails()) {
+                $messages = $validator->messages();
+                $errors = $messages->all(); //convert them into one array
+                return response()->json([
+                    'status' => false,
+                    'reason' => 'Validation Fails',
+                    'messages' => $errors,
+                ], 422);
+            }
+
+            /////////////////////////////////////
+
+            $categ = CategoryModel::find($id);
+            if ($categ) {
+                $categ->update([
+                    'name' => $request->name,
+                ]);
+                return new CategoryResource($categ);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'messages' => "Object Not Found"
+                ], 404);
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            abort(code: 500, message: 'fail to update');
+            //Logs implementation goes down herer
+
+
+            ////////////////////////////////////////////
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
+        try {
+            $categ = CategoryModel::where('id', $id)->delete();
+            if ($categ) {
+                return response()->json([
+                    'status' => true,
+                    'messages' => "Delete Success",
+                    "data" => []
+                ], 200);
+            } else {
+                # code...
+                return response()->json([
+                    'status' => false,
+                    'messages' => "Object Not Found"
+                ], 404);
+            }
+        } catch (\Throwable $th) {
+            //throw $th;
+            abort(code: 500, message: 'fail to delete');
+            //Logs implementation goes down herer
+
+
+            ////////////////////////////////////////////
+        }
     }
 }
