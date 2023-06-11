e2fcd76ef458e8e73f8a9d5ee43216d271cb4b2a
SoftwareGroup2019 <mohamadit69@gmail.com>
2023-06-10 20:48:35 -0700

    CORSFixed

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','http://localhost:3000'],
 
     'allowed_methods' => ['*'],
 
