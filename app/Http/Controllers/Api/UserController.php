6716f0bcd7ba9b84267cbab6ff7cb4a3f9e8db30
SoftwareGroup2019 <mohamadit69@gmail.com>
2023-09-27 19:31:08 -0700

    UserUpdateDone

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\User as UserResource;
 use App\Models\User as UserModel;
+use App\Models\UserTranslation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -50,46 +52,37 @@ class UserController extends Controller
 
     public function update(Request $request, $id)
     {
-        //Validations Rules //////////////////////////
-        // $rules = array(
-        //     'email' => 'required',
-        //     'fullname' => 'required',
-        //     'username' => 'required',
-        //     'fastpay_acc_num' => 'required',
-        //     'phone_number' => 'required',
-        // );
-        /// end of Validation Rules ////////////////////
 
-
-        //Validation Custom Messages
-        // $messages = array('title'=>'All data required');
-
-        // $validator = Validator::make($request->all(), $rules);
-        // if ($validator->fails()) {
-        //     $messages = $validator->messages();
-        //     $errors = $messages->all(); //convert them into one array
-        //     return response()->json([
-        //         'status' => false,
-        //         'reason' => 'Validation Fails',
-        //         'messages' => $errors,
-        //     ], 422);
-        // } else {
-        # put data into DB
         try {
-            // $hashed_pass = array("password" => Hash::make($request->password));
-            // array_merge($request->except(['password']), $hashed_pass)
-            UserModel::where('id', $id)->update($request->all());
+
+            $user_obj = [];
+            $user_trans_obj = [];
+            foreach ($request->all() as $db_feild => $req_feild) {
+                if ($db_feild == "email") {
+                    $user_obj[$db_feild] = $req_feild;
+                } elseif ($db_feild == "password") {
+                    $hashed_req_field = Hash::make($req_feild);
+                    $user_obj[$db_feild] = $hashed_req_field;
+                } elseif ($db_feild == "fastpay_acc_num") {
+                    $user_obj[$db_feild] = $req_feild;
+                } elseif ($db_feild == "phone_number") {
+                    $user_obj[$db_feild] = $req_feild;
+                } elseif ($db_feild == "fullname") {
+                    $user_trans_obj[$db_feild] = $req_feild;
+                } elseif ($db_feild == "username") {
+                    $user_trans_obj[$db_feild] = $req_feild;
+                }
+            }
+
+            UserModel::where('id', $id)->update($user_obj);
+            UserTranslation::where('user_id', $id)->where('locale', App::getLocale())->update($user_trans_obj);
+
             // return User API Resource JSON Response //////////////
             return new UserResource(UserModel::find($id));
             ///////////////////////////////////////////////////////
 
         } catch (\Throwable $th) {
-            //throw $th;
-            // abort(code: 500, message: 'fail to create');
-            return response()->json([
-                'status' => false,
-                'message' => $th->getMessage(),
-            ], 500);
+            throw $th;
         }
         // }
     }
