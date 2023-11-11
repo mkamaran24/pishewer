05d0bfda8588370f581c954ade81b2ed4261e1a6
SoftwareGroup2019 <mohamadit69@gmail.com>
2023-11-11 13:48:02 -0800

    corseUPdated

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:8000', 'https://pishewer.com'],
 
     'allowed_origins_patterns' => [],
 
