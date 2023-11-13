f6bb2a8201f2a13261f263d3f2390c7bb43961a1
SkySoftCompany <skysoft.itcompany@gmail.com>
2023-11-13 10:54:58 +0300

    testcommitV2

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('account/verify/{token}', [AuthController::class, 'verifyAccount'])->name('user.verify');
 
 Route::get('test', function () {
-    return "Hello Kaka Brwa and Kaka Sohaibi Xoshawait";
+    view('welcome');
 });
