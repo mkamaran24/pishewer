cce459d6aaf1b6c1841d09e79403713472c5123d
SkySoftCompany <skysoft.itcompany@gmail.com>
2023-11-13 10:56:57 +0300

    testCommiteV3

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('account/verify/{token}', [AuthController::class, 'verifyAccount'])->name('user.verify');
 
 Route::get('test', function () {
-    view('welcome');
+    return view('welcome');
 });
