76ae0a6ee31c70c8532bf96464196772c55021ad
mkamaran24 <mhamad.00192801@gmail.com>
2023-12-26 15:43:12 +0300

    default_admin_user

---
+++ database/seeders/AdminUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'email' => 'admin@pishewer.com',
+            'password' => Hash::make('ed123123'),
+            'role' => 1
+        ]);
+    }
+}
