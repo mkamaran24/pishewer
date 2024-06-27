50c53b4f087f7aac2f52eed8039e6cf9cb48e9b0
mkamaran24 <mhamad.00192801@gmail.com>
2024-06-27 08:54:59 +0300

    update_EXPIRY_middlware

--- app/Http/Middleware/CheckOfferExpiry.php
+++ app/Http/Middleware/CheckOfferExpiry.php
@@ -22,7 +22,7 @@ class CheckOfferExpiry
      */
     public function handle(Request $request, Closure $next)
     {
-
+        // Update Expiry Middleware
         $response = $next($request);
 
         $user = auth('sanctum')->user();
@@ -33,37 +33,29 @@ class CheckOfferExpiry
             $msg = "Offer Expiry Checked";
             $now = Carbon::now();
 
-            
 
-            $offer_id = DB::table('offers')->select('id')->where('seller_id',$user->id)->orWhere('buyer_id',$user->id)->get();
-            
-            if (!empty($offer_id)) 
-            {
-                foreach($offer_id as $key=>$id)
-                {
-                    $check_attach = DB::table('attachments')->where('offer_id',$id->id)->exists();
-                    
-                    if($check_attach)
-                    {
-    
-                        OfferModel::where('offer_expiry','<=',$now)->where('offer_state','inProgress')->where('id',$id->id)->update(['offer_state'=>'Closed']);
-                    }
-                    else 
-                    {
-                        OfferModel::where('offer_expiry','<=',$now)->where('id',$id->id)->update(['offer_state'=>'Expired']);
+
+            $offer_id = DB::table('offers')->select('id')->where('seller_id', $user->id)->orWhere('buyer_id', $user->id)->get();
+
+            if (!empty($offer_id)) {
+                foreach ($offer_id as $key => $id) {
+                    $check_attach = DB::table('attachments')->where('offer_id', $id->id)->exists();
+
+                    if ($check_attach) {
+
+                        OfferModel::where('offer_expiry', '<=', $now)->where('offer_state', 'inProgress')->where('id', $id->id)->update(['offer_state' => 'Closed']);
+                    } else {
+                        OfferModel::where('offer_expiry', '<=', $now)->where('id', $id->id)->update(['offer_state' => 'Expired']);
                     }
                 }
-            }
-            else
-            {
+            } else {
                 $msg = "Offers Not Found for this User";
-            }    
-        }
-        else{
+            }
+        } else {
             $msg = "Faild to Check Offer Expiry";
         }
 
-       
+
         $response->headers->set('X-Custom-Header', $msg);
 
         return $response;
